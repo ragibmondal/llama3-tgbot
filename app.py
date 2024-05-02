f7a543e177d1fcac555ede3b654083cc8f4667d2
Ragib Mondal <124962194+ragibmondal@users.noreply.github.com>
2024-05-02 14:11:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from telegram_bot import run_telegram_bot
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates', template_folder='templates')
 
 @app.route('/')
 def index():
